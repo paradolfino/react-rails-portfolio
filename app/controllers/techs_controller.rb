d6b67f933fcd11e34f5539a1a741c8695fca2bdf
viktharien <viktharien@zoho.com>
2018-03-19 19:50:26 -0500

    finish setting up rest actions for techs controller

--- app/controllers/techs_controller.rb
+++ app/controllers/techs_controller.rb
@@ -15,6 +15,11 @@ class TechsController < ApplicationController
 
     def create
         @tech = Tech.new(tech_params)
+        if @tech.save
+            redirect_to @tech
+        else
+            render 'new'
+        end
     end
 
     def update
