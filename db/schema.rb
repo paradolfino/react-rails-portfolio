d3eeee719e8d8b65ab8af730ed98808af0bd3e10
viktharien <viktharien@zoho.com>
2018-03-19 19:35:10 -0500

    set up many to one relationship from projects to technology

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320003156) do
+ActiveRecord::Schema.define(version: 20180320003434) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "technology_id"
+    t.index ["technology_id"], name: "index_projects_on_technology_id"
   end
 
   create_table "teches", force: :cascade do |t|
