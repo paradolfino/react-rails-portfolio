d9b5eef1a337b7055dca687f2e2996d1bf6e52cd
viktharien <viktharien@zoho.com>
2018-03-20 19:03:48 -0500

    fixing association

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180320003156) do
+ActiveRecord::Schema.define(version: 20180320003123) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
     t.text "desc"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teches", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
