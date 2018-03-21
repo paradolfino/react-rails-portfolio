f6781a8e3257413fedfa3c3d264dbc32cfaf6ff2
viktharien <viktharien@zoho.com>
2018-03-20 19:10:46 -0500

    fixing association

--- db/migrate/20180321000824_create_projects_and_techs.rb
+++ db/migrate/20180321000824_create_projects_and_techs.rb
@@ -1,12 +1,19 @@
 class CreateProjectsAndTechs < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+      t.string :title
+      t.text :desc
+      t.timestamps
     end
 
     create_table :techs do |t|
+      t.string :name
+      t.timestamps
     end
 
     create_table :projects_techs, id: false do |t|
+      t.belongs_to :project, index: true
+      t.belongs_to :tech, index: true
     end
   end
 end
