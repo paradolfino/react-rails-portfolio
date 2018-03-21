8f2f61410b66ed365d1901d35c95e5deb4894a3b
viktharien <viktharien@zoho.com>
2018-03-20 19:20:38 -0500

    fix techs table - rename

--- db/migrate/20180321000824_create_projects_and_techs.rb
+++ db/migrate/20180321000824_create_projects_and_techs.rb
@@ -6,14 +6,14 @@ class CreateProjectsAndTechs < ActiveRecord::Migration[5.1]
       t.timestamps
     end
 
-    create_table :techs do |t|
+    create_table :technologies do |t|
       t.string :name
       t.timestamps
     end
 
-    create_table :projects_techs, id: false do |t|
+    create_table :projects_technologies, id: false do |t|
       t.belongs_to :project, index: true
-      t.belongs_to :tech, index: true
+      t.belongs_to :technology, index: true
     end
   end
 end
