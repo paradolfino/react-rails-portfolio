5892dce5cd67575f2ea83dc6733c29af3b2bdff5
viktharien <viktharien@zoho.com>
2018-03-21 19:27:04 -0500

    add declaration model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180321000824) do
+ActiveRecord::Schema.define(version: 20180322002552) do
+
+  create_table "declarations", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "entries", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
