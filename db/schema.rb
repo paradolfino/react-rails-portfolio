5f7c5d2597fbe407f1aaff5fe40bd848aa240f7e
viktharien <viktharien@zoho.com>
2018-03-27 19:52:03 -0500

    implement friendly_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328004400) do
+ActiveRecord::Schema.define(version: 20180328005129) do
 
   create_table "declarations", force: :cascade do |t|
     t.text "content"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 20180328004400) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
   end
 
 end
