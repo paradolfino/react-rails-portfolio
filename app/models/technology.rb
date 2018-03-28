051c4c8f5c6781ab9ae750eff0e97882a31ea11e
viktharien <viktharien@zoho.com>
2018-03-27 19:45:38 -0500

    implement friendly_id

--- app/models/technology.rb
+++ app/models/technology.rb
@@ -1,4 +1,6 @@
 class Technology < ApplicationRecord
     has_and_belongs_to_many :projects
+    extend FriendlyId
+    friendly_id :name, use: :slugged
     validates :name, presence: true, length: { minimum: 1 }
 end
