8b236c019a0f976cef6b21227a8d545e031ea5c7
viktharien <viktharien@zoho.com>
2018-04-05 19:57:31 -0600

    set up pages controller for static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,11 +4,6 @@ class PagesController < ApplicationController
 
     end
 
-    def projects
-        @projects = Project.all
-        @technologies = Technology.all
-    end
-
     def contact
 
     end
