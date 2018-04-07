d2aa09e27759ad5d95144c6c0a70093567930552
viktharien <viktharien@zoho.com>
2018-04-07 17:27:40 -0500

    testing react component with data from declarations

--- app/assets/javascripts/components/test.es6.jsx
+++ app/assets/javascripts/components/test.es6.jsx
@@ -8,7 +8,7 @@ class Test extends React.Component {
         return (
             <div>
                 {this.props.text.map((i,k)=> {
-                    return <p>{i.content}</p>
+                    return <p key={k}>{i.content}</p>
                 })}
             </div>
         )
