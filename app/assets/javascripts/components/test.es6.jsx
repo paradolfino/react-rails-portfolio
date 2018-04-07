a6e6b846fee8f839d2b0729e6477aa28ad15a0cd
viktharien <viktharien@zoho.com>
2018-04-07 17:21:44 -0500

    testing react component with data from declarations

--- app/assets/javascripts/components/test.es6.jsx
+++ app/assets/javascripts/components/test.es6.jsx
@@ -2,6 +2,11 @@ class Test extends React.Component {
     constructor(props) {
         super(props);
     }
+
+    getInitialState() {
+        return { items: [] };
+    }
+
     render() {
         contents = this.props.text.map((i)=>{
             return <p>{JSON.stringify(i.content)}</p>;
