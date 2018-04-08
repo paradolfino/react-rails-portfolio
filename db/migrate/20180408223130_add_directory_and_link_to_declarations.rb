31344a8d36a834903aec1e55ffd734dbe35b1a25
viktharien <viktharien@zoho.com>
2018-04-08 17:33:13 -0500

    add columns to declarations model for directory and git link

--- db/migrate/20180408223130_add_directory_and_link_to_declarations.rb
+++ db/migrate/20180408223130_add_directory_and_link_to_declarations.rb
@@ -1,4 +1,6 @@
 class AddDirectoryAndLinkToDeclarations < ActiveRecord::Migration[5.1]
   def change
+    add_column :declaration, :directory, :string
+    add_column :declaration, :git, :string
   end
 end
