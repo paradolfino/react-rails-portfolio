b3d78b87de64a67f9b64afabfac32f0231d9a2d7
viktharien <viktharien@zoho.com>
2018-04-08 17:33:37 -0500

    add columns to declarations model for directory and git link

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328005129) do
+ActiveRecord::Schema.define(version: 20180408223130) do
 
   create_table "declarations", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "directory"
+    t.string "git"
   end
 
   create_table "entries", force: :cascade do |t|
